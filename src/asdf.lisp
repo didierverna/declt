ea114a0b69b7c33c8fa69ee00b2ab3145954ae56
Didier Verna <didier@lrde.epita.fr>
2010-09-03 22:08:35 +0200

    Forgot to add asdf.lisp.

---
+++ src/asdf.lisp
@@ -0,0 +1,39 @@
+;;; asdf.lisp --- ASDF utilities
+
+;; Copyright (C) 2010 Didier Verna
+
+;; Author:        Didier Verna <didier@lrde.epita.fr>
+;; Maintainer:    Didier Verna <didier@lrde.epita.fr>
+;; Created:       Fri Sep  3 21:59:21 2010
+;; Last Revision: Fri Sep  3 21:59:21 2010
+
+;; This file is part of Declt.
+
+;; Declt is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+
+;; Declt is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+;;; Commentary:
+
+;; Contents management by FCM version 0.1.
+
+
+;;; Code:
+
+(in-package :com.dvlsoft.declt)
+
+
+(import '(asdf:find-system))
+
+;;; asdf.lisp ends here
