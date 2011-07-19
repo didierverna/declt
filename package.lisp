8b753791fab7e5bf451495044e563d22b96db503
Didier Verna <didier@lrde.epita.fr>
2011-07-19 16:05:49 +0200

    Add a Declt readtable with ~ and i readers.
    ChangeLog addition:
    
    2011-07-19  Didier Verna  <didier@lrde.epita.fr>
    
    	* package.lisp (:com.dvlsoft.declt): Shadow *READTABLE*.
    	* package.lisp (*readtable*): New variable.
    	* package.lisp (tilde-reader): New reader-macro.
    	* package.lisp (clindent): New function.
    	* package.lisp (defindent): New macro.
    	* package.lisp (i-reader): New reader-macro.
    	* package.lisp (in-readtable): New macro.

--- package.lisp
+++ package.lisp
@@ -4,8 +4,6 @@
 
 ;; Author:        Didier Verna <didier@lrde.epita.fr>
 ;; Maintainer:    Didier Verna <didier@lrde.epita.fr>
-;; Created:       Mon Aug 23 17:41:36 2010
-;; Last Revision: Wed Jun 29 13:49:14 2011
 
 ;; This file is part of Declt.
 
@@ -33,7 +31,10 @@
 (in-package :cl-user)
 
 (defpackage :com.dvlsoft.declt
+  (:documentation
+   "The Documentation Extractor from Common Lisp to Texinfo package.")
   (:use :cl)
+  (:shadow :*readtable*)
   (:import-from :com.dvlsoft.declt.asdf
     :define-constant
     :+release-major-level+
@@ -82,6 +83,67 @@
 		  (package-name :com.dvlsoft.declt)
 		  (adjoin nickname (package-nicknames :com.dvlsoft.declt)
 			  :test #'string-equal)))
+
+
+;; -------------------
+;; Internal utilities:
+;; -------------------
+
+(defvar *readtable* (copy-readtable)
+  "The Declt readtable.")
+
+;; String concatenation
+;; --------------------
+(defun tilde-reader (stream char)
+  "Read a series of ~\"string\" to be concatenated together."
+  (declare (ignore char))
+  (flet ((read-string (&aux (string (read stream t nil t)))
+	   (check-type string string "a string")
+	   string))
+    (apply #'concatenate 'string
+	   (read-string)
+	   (loop :while (char= (peek-char t stream nil nil t) #\~)
+		 :do (read-char stream t nil t)
+		 :collect (read-string)))))
+
+(set-macro-character #\~ #'tilde-reader nil *readtable*)
+
+;; Emacs indentation
+;; -----------------
+(defun clindent (symbol indent)
+  "Set SYMBOL's indentation to INDENT in (X)Emacs.
+This function sets SYMBOL's common-lisp-indent-function property.
+If INDENT is a symbol, use its indentation definition.
+Otherwise, INDENT is considered as an indentation definition."
+  (when (and (member :swank *features*)
+	     (boundp 'cl-user::com.dvlsoft.declt.swank-eval-in-emacs)
+	     cl-user::com.dvlsoft.declt.swank-eval-in-emacs)
+    (funcall (intern "EVAL-IN-EMACS" :swank)
+	     `(put ',symbol 'common-lisp-indent-function
+		   ,(if (symbolp indent)
+			`(get ',indent 'common-lisp-indent-function)
+		      `',indent))
+	     t)))
+
+(defmacro defindent (symbol indent)
+  "Set SYMBOL's indentation to INDENT in (X)Emacs.
+SYMBOL and INDENT need not be quoted.
+See CLINDENT for more information."
+  `(eval-when (:compile-toplevel :execute :load-toplevel)
+     (clindent ',symbol ',indent)))
+
+(defun i-reader (stream subchar arg)
+  "Read an argument list for the DEFINDENT macro."
+  (declare (ignore subchar arg))
+  (cons 'defindent (read stream)))
+
+(set-dispatch-macro-character #\# #\i #'i-reader *readtable*)
+
+
+(defmacro in-readtable (name)
+  "Set the current readtable to the value of NAME::*READTABLE*."
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (setf cl:*readtable* (symbol-value (find-symbol "*READTABLE*" ,name)))))
 
 
 ;;; package.lisp ends here
