8ae5552eaa30d0c3df439aac1fd899434f7ca3a7
Didier Verna <didier@lrde.epita.fr>
2012-01-09 10:18:20 +0100

    Update local indentation support.
    ChangeLog addition:
    
    2012-01-09  Didier Verna  <didier@lrde.epita.fr>
    
    	* package.lisp (clindent): Use configuration variable
    	'com.dvlsoft.declt.configuration. Look for :swank-eval-in-emacs
    	option inside it.

--- package.lisp
+++ package.lisp
@@ -116,8 +116,11 @@ This function sets SYMBOL's common-lisp-indent-function property.
 If INDENT is a symbol, use its indentation definition.
 Otherwise, INDENT is considered as an indentation definition."
   (when (and (member :swank *features*)
-	     (boundp 'cl-user::com.dvlsoft.declt.swank-eval-in-emacs)
-	     cl-user::com.dvlsoft.declt.swank-eval-in-emacs)
+	     (let ((configuration
+		     (find-symbol "COM.DVLSOFT.DECLT.CONFIGURATION"
+				  :cl-user)))
+	       (when (and configuration (boundp configuration))
+		 (getf (symbol-value configuration) :swank-eval-in-emacs))))
     (funcall (intern "EVAL-IN-EMACS" :swank)
 	     `(put ',symbol 'common-lisp-indent-function
 		   ,(if (symbolp indent)
