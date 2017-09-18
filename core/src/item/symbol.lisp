didierverna/declt

commit 38f620d5be64552bedb1b6a58870a4304c844016

Update support for SBCL's setf expanders introspection.
This is borrowed from a recent Slime.

2017-09-17  Didier Verna  <didier@didierverna.net>

	* core/src/item/symbol.lisp (boolean-to-feature-expression):
	(sbcl-has-setf-inverse-meta-info): New functions.
	(setf-expander-p): Use the latter.

core/src/item/symbol.lisp
@@ -466,13 +466,25 @@ Note that this only returns standalone (toplevel) generic writers."
 	     :slot slot))
 	  (sb-mop:class-direct-slots class)))
 
-;; Stolen from Slime:
-;; github.com/slime/slime/commit/51997f5eedb529969f223f14a80a0c14d49eca59
+;; 3 functions stolen/adapted from Slime:
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun boolean-to-feature-expression (value)
+    "Convert boolean VALUE to a form suitable for feature testing."
+    (if value '(:and) '(:or)))
+  ;; #### PORTME.
+  (defun sbcl-has-setf-inverse-meta-info ()
+    (boolean-to-feature-expression
+     ;; going through FIND-SYMBOL since META-INFO was renamed from
+     ;; TYPE-INFO in 1.2.10.
+     (let ((sym (find-symbol "META-INFO" "SB-C")))
+       (and sym
+	    (fboundp sym)
+	    (funcall sym :setf :inverse ()))))))
 ;; #### PORTME.
-(defun setf-expander-p (symbol)
+(defun setf-expander (symbol)
   "Return whether SYMBOL defines a setf-expander."
   (or
-   #+#.(cl:if (sb-c::meta-info :setf :inverse ()) '(:and) '(:or))
+   #+#.(net.didierverna.declt::sbcl-has-setf-inverse-meta-info)
    (sb-int:info :setf :inverse symbol)
    (sb-int:info :setf :expander symbol)))
 

commit 12fc9c5bf30e8ecd1a3a1fdd8aedf8fedf827629

Fix bug introduced by commit 38f620d5be64552bedb1b6a58870a4304c844016.
2017-09-18  Didier Verna  <didier@didierverna.net>

	* core/src/item/symbol.lisp (setf-expander): Rename to
	SETF-EXPANDER-P.

core/src/item/symbol.lisp
@@ -480,7 +480,7 @@ Note that this only returns standalone (toplevel) generic writers."
 	    (fboundp sym)
 	    (funcall sym :setf :inverse ()))))))
 ;; #### PORTME.
-(defun setf-expander (symbol)
+(defun setf-expander-p (symbol)
   "Return whether SYMBOL defines a setf-expander."
   (or
    #+#.(net.didierverna.declt::sbcl-has-setf-inverse-meta-info)