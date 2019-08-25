didierverna/declt

commit cbfac166b6b023af17eca7fdfe4ada2921473bdc

Increase the robustness of contact computation.
Some people feel the need for empty strings as authors or maintainers
in ASDF system descriptions. Protect against that.

2019-08-24  Didier Verna  <didier@didierverna.net>

	* core/src/util/misc.lisp (parse-contact-string): Return empty
	string instead of NIL for no email.
	* core/src/declt.lisp (declt): Filter out empty strings from
	contacts.

core/src/declt.lisp
@@ -596,7 +596,9 @@ INTRODUCTION and CONCLUSION are currently expected to be in Texinfo format."
 	  ((consp (system-maintainer system))
 	   (setq contact (append (system-maintainer system) contact))))
     (unless contact (setq contact (list "John Doe"))))
-  (setq contact (remove-duplicates contact :from-end t :test #'string=))
+  (setq contact
+	(remove-if (lambda (string) (zerop (length string)))
+		   (remove-duplicates contact :from-end t :test #'string=)))
   (multiple-value-bind (names emails) (|parse-contact(s)| contact)
     (setq contact-names names
 	  contact-emails emails))

core/src/util/misc.lisp
@@ -65,16 +65,15 @@
       year
       (- tz))))
 
-;; Stolen from Tinaa.
 (defun parse-contact-string
     (string &aux (pos-< (position #\< string :test #'char-equal))
 		 (pos-> (position #\> string :test #'char-equal)))
   "Parse STRING as \"NAME <EMAIL>\".
 Return NAME and EMAIL as two values."
   (if (and pos-< pos-> (< pos-< pos->))
-      (values (if (zerop pos-<) "" (subseq string 0 (1- pos-<)))
-	      (subseq string (1+ pos-<) pos->))
-    string))
+    (values (if (zerop pos-<) "" (subseq string 0 (1- pos-<)))
+	    (subseq string (1+ pos-<) pos->))
+    (values string "")))
 
 (defun |parse-contact(s)| (|contact(s)|)
   "Parse CONTACT(S) as either a contact string, or a list of such.

commit d245cf637eb1987cc437e71cc30d194c84d2e4a4

Fix contact management again.
Improve the implementation and fix a new bug introduced by commit
cbfac166b6b023af17eca7fdfe4ada2921473bdc.

2019-08-25  Didier Verna  <didier@didierverna.net>

	* core/src/util/misc.lisp (parse-contact-string): Use NIL instead
	of empty strings for missing names or emails.
	* core/src/declt.lisp (declt): Update accordingly. Also verify
	that SYSTEM-MAILTO is a one liner before using it.
	(render-header): Update accordingly.

core/src/declt.lisp
@@ -127,7 +127,8 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.")))
       ;; list, but there may still be duplicates in the names, for instance if
       ;; somebody used his name several times, with a different email
       ;; address.
-      (remove-duplicates contact-names :from-end t :test #'string=))
+      (remove-duplicates (remove-if #'null contact-names)
+        :from-end t :test #'string=))
     (terpri))
 
   (format t "@c This file is part of ~A.~2%" library-name)
@@ -413,7 +414,8 @@ The ~A Reference Manual~@[, version ~A~].
 	;; contact list, but there may still be duplicates in the names, for
 	;; instance if somebody used his name several times, with a different
 	;; email address.
-	(remove-duplicates contact-names :from-end t :test #'string=))
+	(remove-duplicates (remove-if #'null contact-names)
+          :from-end t :test #'string=))
       (terpri))
     (format t "~
 Permission is granted to make and distribute verbatim copies of this
@@ -608,7 +610,7 @@ INTRODUCTION and CONCLUSION are currently expected to be in Texinfo format."
   (when (and (= (length contact-names) 1)
 	     (not contactp)
 	     (null (car contact-emails))
-	     (system-mailto system))
+	     (one-liner-p (system-mailto system)))
     (setq contact-emails (list (system-mailto system))))
 
   (setq copyright-years

core/src/util/misc.lisp
@@ -69,11 +69,11 @@
     (string &aux (pos-< (position #\< string :test #'char-equal))
 		 (pos-> (position #\> string :test #'char-equal)))
   "Parse STRING as \"NAME <EMAIL>\".
-Return NAME and EMAIL as two values."
+Return NAME and EMAIL, as two potentially NIL values."
   (if (and pos-< pos-> (< pos-< pos->))
-    (values (if (zerop pos-<) "" (subseq string 0 (1- pos-<)))
+    (values (if (zerop pos-<) nil (subseq string 0 (1- pos-<)))
 	    (subseq string (1+ pos-<) pos->))
-    (values string "")))
+    (values string nil)))
 
 (defun |parse-contact(s)| (|contact(s)|)
   "Parse CONTACT(S) as either a contact string, or a list of such.