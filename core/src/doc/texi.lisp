f4c8be99fceb5bcafd972c3ab0ff1ea63e0cd38a
Didier Verna <didier@didierverna.net>
2021-04-07 20:13:42 +0200

    File header maintenance.
    2021-04-07  Didier Verna  <didier@didierverna.net>
    
    	* core/src/doc/texi.lisp:
    	* core/src/doc/symbol.lisp:
    	* core/src/doc/package.lisp:
    	* core/src/doc/asdf.lisp:
    	* core/src/declt.lisp: Update copyright years.

--- core/src/doc/texi.lisp
+++ core/src/doc/texi.lisp
@@ -1,6 +1,6 @@
 ;;; texi.lisp --- Texinfo format utilities
 
-;; Copyright (C) 2010-2013, 2015-2017, 2019 Didier Verna
+;; Copyright (C) 2010-2013, 2015-2017, 2019, 2021 Didier Verna
 
 ;; Author: Didier Verna <didier@didierverna.net>
 
