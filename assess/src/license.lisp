97db5d2ca7a61a5f5a923613d4f15275909c7205
Didier Verna <didier@didierverna.net>
2022-05-02 10:58:28 +0200

    Small cosmetic fix.
    2022-05-02  Didier Verna  <didier@didierverna.net>
    
    	* assess/src/license.lisp (*licenses*): Identation cleanup.

--- assess/src/license.lisp
+++ assess/src/license.lisp
@@ -97,9 +97,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.")
-      (:lgpl
-       "The GNU LGPL License"
-       "This program is free software; you can redistribute it and/or modify
+    (:lgpl
+     "The GNU LGPL License"
+     "This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
